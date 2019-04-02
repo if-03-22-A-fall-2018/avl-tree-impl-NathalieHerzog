94956fe4f08f9990cbfc7bf88ad099d710292154
NathalieHerzog <nathalieherzog2003@gmail.com>
2019-04-02 11:40:45 +0200

    ;-;

---
+++ node.cpp
@@ -0,0 +1,44 @@
+#include "node.h"
+#include <stdio.h>
+
+struct Node *head;
+
+int 	get_key (Node node)
+{
+  return 0;
+}
+
+Node 	get_left (Node node)
+{
+  return 0;
+}
+
+void 	set_left (Node node, Node left_node)
+{
+
+}
+
+Node 	get_right (Node node)
+{
+  return 0;
+}
+
+void 	set_right (Node node, Node right_node)
+{
+
+}
+
+int 	get_height (Node node)
+{
+  return 0;
+}
+
+void 	set_height (Node node, int height)
+{
+
+}
+
+Node 	create_node (int key)
+{
+  return 0;
+}
--- node.h
+++ node.h
@@ -1,5 +1,25 @@
 #ifndef BALANCEDTREE_NODE_H
 #define BALANCEDTREE_NODE_H
 
+typedef struct TreeImplementation* Tree;
+
+struct TreeImplementation
+{
+    struct Node *head;
+};
+
+struct Node
+{
+    struct Node *next;
+};
+
+int 	get_key (Node node);
+Node 	get_left (Node node);
+void 	set_left (Node node, Node left_node);
+Node 	get_right (Node node);
+void 	set_right (Node node, Node right_node);
+int 	get_height (Node node);
+void 	set_height (Node node, int height);
+Node 	create_node (int key);
 
 #endif //BALANCEDTREE_NODE_H
